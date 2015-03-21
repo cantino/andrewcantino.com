f05b0c9ec139b7a64b0aef69509a5617faa0705c
Andrew Cantino <cantino@gmail.com>
2015-03-21 15:02:18 -0700

    fix mime-type guessing

--- bin/upload_to_s3.sh
+++ bin/upload_to_s3.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-s3cmd sync --cf-invalidate --delete-removed --rexclude=^.git\|^bin . s3://andrewcantino.com
+s3cmd sync --no-check-md5 --cf-invalidate -M --no-mime-magic --delete-removed --rexclude=^.git\|^bin . s3://andrewcantino.com
