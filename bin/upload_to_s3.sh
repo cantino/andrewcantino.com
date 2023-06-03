7a7fe9b6050b5cb216656b6f14c65751e9d7142e
Andrew Cantino <cantino@users.noreply.github.com>
2023-06-03 14:02:55 -0700

    Fix upload

--- bin/upload_to_s3.sh
+++ bin/upload_to_s3.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-s3cmd sync --no-check-md5 --cf-invalidate -M --no-mime-magic --delete-removed --rexclude=^.git\|^bin . s3://andrewcantino.com
+s3cmd sync --no-check-md5 --cf-invalidate -M --no-mime-magic --delete-removed --rexclude=^.git\|.idea\|^bin\|^mac/sympatric-speciation . s3://andrewcantino.com
