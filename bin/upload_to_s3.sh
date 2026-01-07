19299d1f9e51368e8759b2ded5c9bd282dc13ad0
Andrew Cantino <cantino@users.noreply.github.com>
2026-01-07 15:52:20 -0800

    Update

--- bin/upload_to_s3.sh
+++ bin/upload_to_s3.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-s3cmd sync --no-check-md5 --cf-invalidate -M --no-mime-magic --delete-removed --rexclude=^.git\|.idea\|^bin\|^mac/sympatric-speciation . s3://andrewcantino.com
+s3cmd sync --no-check-md5 --cf-invalidate -M --no-mime-magic --delete-removed --rexclude=^.git\|^.idea\|.DS_Store\|^bin\|^mac/sympatric-speciation . s3://andrewcantino.com
